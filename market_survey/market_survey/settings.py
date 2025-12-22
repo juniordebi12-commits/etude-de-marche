ab0fc55b64e1c9eaba5ce7c11d20bde7eb6a7d06
junior DEBI <juniordebi12@gmail.com>
2025-12-22 20:10:42 +0100

    Fix production settings for Render (STATIC_ROOT, ALLOWED_HOSTS, DEBUG)

--- market_survey/market_survey/settings.py
+++ market_survey/market_survey/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jansja$f=y22&ndr-!t84t*ax1+3ekt8dqd4$8p3nl9y+^p*eo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -145,6 +145,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
