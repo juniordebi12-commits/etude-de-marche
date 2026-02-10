16d1adc933dafe45527ba9c77295afc715e2b838
junior DEBI <juniordebi12@gmail.com>
2026-02-10 23:35:28 +0100

    Fix urls and setting

--- market_survey/market_survey/settings.py
+++ market_survey/market_survey/settings.py
@@ -92,7 +92,7 @@ ROOT_URLCONF = 'market_survey.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- market_survey/market_survey/urls.py
+++ market_survey/market_survey/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     # Django admin
@@ -27,3 +29,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    path("<path:path>", TemplateView.as_view(template_name="index.html")),
+]
