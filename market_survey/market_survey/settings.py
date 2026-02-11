5c2432dedce9064c11e76544a9c6a421f8d2aa97
junior DEBI <juniordebi12@gmail.com>
2026-02-11 23:31:41 +0100

    Fix settings.py

--- market_survey/market_survey/settings.py
+++ market_survey/market_survey/settings.py
@@ -92,7 +92,7 @@ ROOT_URLCONF = 'market_survey.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [BASE_DIR / "survey" / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
